b221e6dfefeccb0b8b3c3f9de95929df17609c12
Duc Duy <0374740747s@gmail.com>
2021-03-11 00:03:52 +0700

    feat: add tailwind css, ant design

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AuthLayoutComponent, MainLayoutComponent],
   imports: [
     CommonModule
   ]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import vi from '@angular/common/locales/ja';
+import { IconsProviderModule } from './icons-provider.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+// ant design
+import { vi_VN, NZ_I18N } from 'ng-zorro-antd/i18n';
+import { HttpClientModule } from '@angular/common/http';
 
+// Contains all shared modules
+const MODULES: any[] = [
+  CommonModule,
+  IconsProviderModule,
+  FormsModule,
+  ReactiveFormsModule,
+  HttpClientModule
+];
+
+// Contains all shared components
+const COMPONENTS: any[] = [];
+
+// Contains all shared pipes
+const PIPES: any[] = [];
+
+// Contains all shared directives
+const DIRECTIVES: any[] = [];
+
+// Contains all shared services
+const SERVICES: any[] = [];
+
+registerLocaleData(vi);
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [...COMPONENTS, ...DIRECTIVES, ...PIPES],
+  imports: [...MODULES],
+  exports: [...MODULES, ...COMPONENTS, ...DIRECTIVES, ...PIPES],
+  providers: [...SERVICES, { provide: NZ_I18N, useValue: vi_VN }]
 })
-export class SharedModule { }
+export class SharedModule {}
